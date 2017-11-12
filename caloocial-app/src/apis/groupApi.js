5d829946cd7f8faac55da957190db358f843b62d
Rottenhoffer Oliver <dvdrip.xvid.avi@gmail.com>
2017-11-12 14:17:20 +0100

    use axios instead of fetch

--- caloocial-app/src/apis/groupApi.js
+++ caloocial-app/src/apis/groupApi.js
@@ -1,20 +1,20 @@
 import config from "../config";
+import axios from "axios";
 
 const host = config.apiGateway + "/usergroups";
 
 export async function getGroupsByPerson(personId) {
-  let response = await fetch(`${host}/persons/${personId}/groups`);
+  let response = await axios.get(`${host}/persons/${personId}/groups`);
   if (response.status !== 200) {
       throw Error("Failed to fetch groups");
   }
-  return await response.json();
+  return await response.data;
 }
 
 export async function getGroupDetails(groupId){
-  let response = await fetch(`${host}/groups/${groupId}`);
-  console.log(response);
+  let response = await axios.get(`${host}/groups/${groupId}`);
   if(response.status !== 200){
     throw Error("Failed to fetch group details");
   }
-  return await response.json();
+  return await response.data;
 }
