dd03cab4370c380008290efb2bbaf7018c0dde45
Rottenhoffer Oliver <dvdrip.xvid.avi@gmail.com>
2017-11-12 14:17:39 +0100

    create userApi.js

--- caloocial-app/src/apis/userApi.js
+++ caloocial-app/src/apis/userApi.js
@@ -1,3 +1,36 @@
 import config from "../config";
+import axios from "axios";
 
 const host = config.apiGateway + "/uaa";
+
+export async function login(username, passwordHash) {
+  return await axios.post(
+    host + "/oauth/token",
+    {},
+    {
+      headers: { Authorization: "Basic YnJvd3Nlcjo=" },
+      params: {
+        username: username,
+        password: passwordHash,
+        scope: "ui",
+        grant_type: "password"
+      }
+    }
+  );
+}
+
+export async function getPrincipal() {
+  return await axios.get(host + "/users/current");
+}
+
+export async function refreshToken(refreshToken) {
+  return await axios.post(
+    host + "/oauth/token",
+    {},
+    {
+      params: {
+        refresh_token: refreshToken
+      }
+    }
+  );
+}
