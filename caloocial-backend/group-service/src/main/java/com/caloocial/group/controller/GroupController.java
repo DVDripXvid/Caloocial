e30633aa73ed3e74e03b63e384bc604a43cc4315
Rottenhoffer Oliver <dvdrip.xvid.avi@gmail.com>
2017-12-02 13:38:35 +0100

    IsAdminInGroup security

--- caloocial-backend/group-service/src/main/java/com/caloocial/group/controller/GroupController.java
+++ caloocial-backend/group-service/src/main/java/com/caloocial/group/controller/GroupController.java
@@ -3,6 +3,7 @@ package com.caloocial.group.controller;
 import com.caloocial.group.domain.Group;
 import com.caloocial.group.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
@@ -24,25 +25,30 @@ public class GroupController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{groupId}")
+    @PreAuthorize("@groupSecurity.isAdminInGroup(authentication, #groupId)")
     public void deleteGroup(@PathVariable long groupId){
         groupService.deleteGroup(groupId);
     }
 
+    @PreAuthorize("@groupSecurity.isAdminInGroup(authentication, #groupId)")
     @RequestMapping(method = RequestMethod.POST, value = "/{groupId}/members")
     public void addMemberToGroup(@PathVariable long groupId, @RequestParam long personId){
         groupService.addMember(groupId, personId);
     }
 
+    @PreAuthorize("@groupSecurity.isAdminInGroup(authentication, #groupId)")
     @RequestMapping(method = RequestMethod.PUT, value = "/{groupId}/members/{personId}/promote")
     public void promoteMember(@PathVariable long groupId, @PathVariable long personId){
         groupService.promoteMemberToAdmin(groupId, personId);
     }
 
+    @PreAuthorize("@groupSecurity.isAdminInGroup(authentication, #groupId)")
     @RequestMapping(method = RequestMethod.PUT, value = "/{groupId}/members/{personId}/demote")
     public void demoteMember(@PathVariable long groupId, @PathVariable long personId){
         groupService.demoteAdminToMember(groupId, personId);
     }
 
+    @PreAuthorize("@groupSecurity.isAdminInGroup(authentication, #groupId)")
     @RequestMapping(method = RequestMethod.DELETE, value = "/{groupId}/members")
     public void removeMemberFromGroup(@PathVariable long groupId, @RequestParam long personId){
         groupService.removeMember(groupId, personId);
